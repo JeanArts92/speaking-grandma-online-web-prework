2b668c22db5d19fb376e1423a1b4a6a9e12bd642
Reginald Jean Amedee <reginald.amedee@gmail.com>
2019-04-07 12:53:51 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -1,6 +1,6 @@
 def speak_to_grandma(string = "I LOVE YOU TOO PUMPKIN!")
   if string == "I love you grandma!".upcase
-    puts "I LOVE YOU TOO PUMPKIN!"
+    return "I LOVE YOU TOO PUMPKIN!"
   end
 end 
   
