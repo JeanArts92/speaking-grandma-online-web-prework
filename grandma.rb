2ee6fbe9e196f6521031bf54089325bdfaa79520
Reginald Jean Amedee <reginald.amedee@gmail.com>
2019-04-07 13:00:25 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -1,7 +1,14 @@
 def speak_to_grandma(string = "I LOVE YOU TOO PUMPKIN!")
   if string == "I love you grandma!".upcase
     return "I LOVE YOU TOO PUMPKIN!"
-    elsif string == 
+    elsif string == "Hi Nana, how are you?"
+    return "Huh?! Speak up, Sonny!".upcase
+    elsif string == "Hi!"
+    return "Huh?! Speak up, Sonny!".upcase
+  elsif string == "WHAT DID YOU EAT TODAY?"
+    return "NO, NOT SINCE 1938!"
+  else string == "WHAT?"
+    return "NO, NOT SINCE 1938!"
   end
 end 
   
