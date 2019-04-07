faf58030d514af1237f8190309221582abc7c010
Reginald Jean Amedee <reginald.amedee@gmail.com>
2019-04-07 12:43:21 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -1,11 +1,11 @@
 def speak_to_grandma(string = "I LOVE YOU TOO PUMPKIN!")
-  if string == "I LOVE YOU GRANDMA!"
+  if string = "I LOVE YOU GRANDMA!"
     puts "I LOVE YOU TOO PUMPKIN!"
-    elsif string == "Hi nana, how are you?"
+    elsif string = "Hi nana, how are you?"
     puts "HUH!? SPEAK UP SUNNY!"
-    elsif string == "WHAT DID YOU EAT TODAY"
+    elsif string = "WHAT DID YOU EAT TODAY"
     puts "NO, NOT SINCE 1938!"
-    else string == "WHAT"
+    else string = "WHAT"
     puts "NO, NOT SINCE 1938!"
   end
 end 
