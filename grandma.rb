35a5794fa95ea00a954e08a8dd3e1c79dbbb87a3
Reginald Jean Amedee <reginald.amedee@gmail.com>
2019-04-07 12:46:42 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -1,12 +1,5 @@
-def speak_to_grandma(string = "I LOVE YOU TOO PUMPKIN!")
-  if string = "I LOVE YOU GRANDMA!"
-    puts "I LOVE YOU TOO PUMPKIN!"
-    elsif string = "Hi nana, how are you?"
-    puts "HUH!? SPEAK UP SUNNY!"
-    elsif string = "WHAT DID YOU EAT TODAY"
-    puts "NO, NOT SINCE 1938!"
-    else string = "WHAT"
-    puts "NO, NOT SINCE 1938!"
-  end
+def speak_to_grandma(string = "I love you grandma!")
+  if string == "I love you grandma!".upcase
+    puts "Huh!?"
 end 
   
